da787d51e890e986d8f066e6e51a817c43e31888
lbwcc <2710356949@qq.com>
2025-05-27 14:45:15 +0800

    🔧 build:

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -78,7 +78,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/vue-beginner/'),
+  history: createWebHashHistory('/vue-beginner/'),
   routes
 })
 
--- vite.config.mjs
+++ vite.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
       }
     }
   },
-  base: './',
+  base: '/vue-beginner/',
 })
 
 
